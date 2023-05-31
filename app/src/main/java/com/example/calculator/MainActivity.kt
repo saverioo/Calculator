8a8fe361b89748d84e3c81bcf3eb445813271a3c
Saverio <saverio.franzese002@studenti.uniparthenope.it>
2023-05-31 18:51:17 +0200

    fixed some errors

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     // FLAG
     private var canAddOperation = false
     private var canAddDecimal = true
+    @SuppressLint("CutPasteId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,85 +23,86 @@ class MainActivity : AppCompatActivity() {
         val operands = findViewById<TextView>(R.id.tvOperands)
         val result = findViewById<TextView>(R.id.tvResult)
 
-        val b0 = findViewById<Button>(R.id.b0)
-        val b1 = findViewById<Button>(R.id.b1)
-        val b2 = findViewById<Button>(R.id.b2)
-        val b3 = findViewById<Button>(R.id.b3)
-        val b4 = findViewById<Button>(R.id.b4)
-        val b5 = findViewById<Button>(R.id.b5)
-        val b6 = findViewById<Button>(R.id.b6)
-        val b7 = findViewById<Button>(R.id.b7)
-        val b8 = findViewById<Button>(R.id.b8)
-        val b9 = findViewById<Button>(R.id.b9)
-        val bcomma = findViewById<Button>(R.id.bComma)
-        val bsum = findViewById<Button>(R.id.bSum)
-        val bsub = findViewById<Button>(R.id.bSubstraction)
-        val bmul = findViewById<Button>(R.id.bMultiplication)
-        val bdiv = findViewById<Button>(R.id.bDivision)
-        val bperc = findViewById<Button>(R.id.bPercentage)
-
         // SETUP NUMBER BUTTONS
+
+        val b0 = findViewById<Button>(R.id.b0)
         b0.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b0), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b1 = findViewById<Button>(R.id.b1)
         b1.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b1), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b2 = findViewById<Button>(R.id.b2)
         b2.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b2), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b3 = findViewById<Button>(R.id.b3)
         b3.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b3), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b4 = findViewById<Button>(R.id.b4)
         b4.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b4), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b5 = findViewById<Button>(R.id.b5)
         b5.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b5), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b6 = findViewById<Button>(R.id.b6)
         b6.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b6), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b7 = findViewById<Button>(R.id.b7)
         b7.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b7), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b8 = findViewById<Button>(R.id.b8)
         b8.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b8), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val b9 = findViewById<Button>(R.id.b9)
         b9.setOnClickListener() {
             operands.text = buttonNumber(findViewById<Button>(R.id.b9), findViewById<TextView>(R.id.tvOperands))
         }
 
         // SETUP OPERATION BUTTONS
+
+        val bsum = findViewById<Button>(R.id.bSum)
         bsum.setOnClickListener() {
             operands.text = buttonOperation(findViewById<Button>(R.id.bSum), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val bsub = findViewById<Button>(R.id.bSubtraction)
         bsub.setOnClickListener() {
-            operands.text = buttonOperation(findViewById<Button>(R.id.bSubstraction), findViewById<TextView>(R.id.tvOperands))
+            operands.text = buttonOperation(findViewById<Button>(R.id.bSubtraction), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val bmul = findViewById<Button>(R.id.bMultiplication)
         bmul.setOnClickListener() {
             operands.text = buttonOperation(findViewById<Button>(R.id.bMultiplication), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val bdiv = findViewById<Button>(R.id.bDivision)
         bdiv.setOnClickListener() {
             operands.text = buttonOperation(findViewById<Button>(R.id.bDivision), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val bcomma = findViewById<Button>(R.id.bComma)
         bcomma.setOnClickListener() {
             operands.text = buttonOperation(findViewById<Button>(R.id.bComma), findViewById<TextView>(R.id.tvOperands))
         }
 
+        val bperc = findViewById<Button>(R.id.bPercentage)
         bperc.setOnClickListener(){
             operands.text = buttonOperation(findViewById<Button>(R.id.bPercentage), findViewById<TextView>(R.id.tvOperands))
         }
@@ -109,15 +112,25 @@ class MainActivity : AppCompatActivity() {
         bac.setOnClickListener() {
             operands.text = ""
             result.text = ""
+            canAddOperation = false
         }
 
         //SETUP BACKSPACE BUTTON
         val bbs = findViewById<Button>(R.id.bBackSpace)
         bbs.setOnClickListener() {
             val length = operands.text.length
-            if (length > 0) {
+            if (length > 1) {
+                val l: String
+                l = operands.text.toString()
+                val c = l[length-2]
                 operands.text = operands.text.subSequence(0, length - 1)
-                canAddOperation = true
+                //canAddOperation è false se c è = a uno di quei caratteri
+                canAddOperation = !(c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '.')
+            }
+            else {
+                canAddOperation = false
+                operands.text = ""
+                result.text = ""
             }
         }
         //SETUP EQUALS BUTTON WITH MATHS API
@@ -125,7 +138,7 @@ class MainActivity : AppCompatActivity() {
         val bequ = findViewById<Button>(R.id.bEquals)
         bequ.setOnClickListener() {
             val queue = Volley.newRequestQueue(this)
-            var str = URLEncoder.encode(operands.text.toString(),"UTF-8")
+            val str = URLEncoder.encode(operands.text.toString(),"UTF-8")
             val url = "https://api.mathjs.org/v4/?expr=${str}"
 
             val stringRequest = StringRequest(
@@ -140,8 +153,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun buttonNumber(b: Button, op: TextView): String {
-            if (op.text == ".") {
+            //if (op.text == ".") {
                 if (canAddDecimal) {
                     when (b.text) {
                         "0" -> op.text = op.text.toString() + "0"
@@ -156,12 +170,13 @@ class MainActivity : AppCompatActivity() {
                         "9" -> op.text = op.text.toString() + "9"
                     }
                 }
-                canAddDecimal = false
-            } else
-                op.text = op.text.toString() + b.text
+               // canAddDecimal = false
+           // } else
+                //op.text = op.text.toString() + b.text
             canAddOperation = true
             return op.text.toString()
         }
+        @SuppressLint("SetTextI18n")
         private fun buttonOperation(b: Button, op: TextView): String{
             if (canAddOperation) {
                 when (b.text) {
@@ -170,6 +185,7 @@ class MainActivity : AppCompatActivity() {
                     "*" -> op.text = op.text.toString() + "*"
                     "/" -> op.text = op.text.toString() + "/"
                     "%" -> op.text = op.text.toString() + "%"
+                    "." -> op.text = op.text.toString() + "."
                 }
                 canAddOperation = false
                 canAddDecimal = true
